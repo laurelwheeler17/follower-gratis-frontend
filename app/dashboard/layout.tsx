bb77a5c79e610059d99fddb8155a06aa071a4598
kiyakebe <kiyakebe799@gmail.com>
2025-09-19 14:23:22 +0300

    Add metadata for SEO to dashboard layout component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,11 @@
 import UserLayout from "@/components/UserLayout";
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard | FollowerGratis.it",
+};
+
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return <UserLayout>{children}</UserLayout>;
